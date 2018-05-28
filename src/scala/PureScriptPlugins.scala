d4983f4cddada5b5372644b6e7a21f725f0a8167
Michael Bayne <mdb@samskivert.com>
2018-05-28 07:40:21 -0700

    Handle projects using psc-package.

--- src/scala/PureScriptPlugins.scala
+++ src/scala/PureScriptPlugins.scala
@@ -4,14 +4,48 @@
 
 package scaled.project
 
+import com.eclipsesource.json._
 import java.nio.file.{Files, Path}
 import scaled._
 
 object PureScriptPlugins {
 
-  val ProjectFiles = Seq("bower.json", "psc-package.json")
+  val PscPkgFile = "psc-package.json"
+  val ProjectFiles = Seq("bower.json", PscPkgFile)
   val NodeModules = "node_modules"
   val PLSModule = "purescript-language-server"
+
+  @Plugin(tag="project-root")
+  class PscPkgRootPlugin extends RootPlugin.File(PscPkgFile)
+
+  @Plugin(tag="project-resolver")
+  class PscPkgResolverPlugin extends ResolverPlugin {
+
+    override def metaFiles (root :Project.Root) = Seq(root.path.resolve(PscPkgFile))
+
+    def addComponents (project :Project) {
+      val rootPath = project.root.path
+      val bowerFile = rootPath.resolve(PscPkgFile)
+      val config = Json.parse(Files.newBufferedReader(bowerFile)).asObject
+
+      val projName = Option(config.get("name")).map(_.asString).
+        getOrElse(rootPath.getFileName.toString)
+
+      val sb = Ignorer.stockIgnores
+      Option(config.get("ignore")).map(_.asArray).foreach { ignores =>
+        // TODO: handle glob ignores properly
+        ignores.map(_.asString).foreach { sb += Ignorer.ignoreName(_) }
+      }
+      project.addComponent(classOf[Filer], new DirectoryFiler(project, sb))
+
+      // TODO: bower doesn't define source directories, so we hack some stuff
+      val sourceDirs = Seq("src", "test").map(rootPath.resolve(_))
+      project.addComponent(classOf[Sources], new Sources(sourceDirs))
+
+      val oldMeta = project.metaV()
+      project.metaV() = oldMeta.copy(name = projName)
+    }
+  }
 
   @Plugin(tag="langserver")
   class PureScriptLangPlugin extends LangPlugin {
@@ -24,8 +58,9 @@ object PureScriptPlugins {
   }
 
   private def serverCmd (root :Path) :Seq[String] = {
-    // TODO: other config
-    val config = """{"autoStartPscIde": true}"""
+    val havePscPkg = Files.exists(root.resolve(PscPkgFile))
+    val config = s"""{"purescript": {"addPscPackageSources": $havePscPkg}}"""
+    // TODO: other config?
 
     Seq("node",
         root.resolve(NodeModules).resolve(PLSModule).resolve("cli.js").toString,
